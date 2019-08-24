35102b7bb10b3f98ed9f168baa3f7a53451c41ad
Oleksandra Sildushkina <regmesmal@gmail.com>
2019-08-24 14:04:48 +0200

    Create classes for user and repo

--- Project/Source Files/DataModel/Repository.swift
+++ Project/Source Files/DataModel/Repository.swift
@@ -7,5 +7,15 @@
 import Foundation
 
 class Repository {
+    var name : String
+    var description : String?
+    var language : String
+    var lastUpdateOn : Date
     
+    init(name: String, description : String, language: String, lastUpdateOn: Date) {
+        self.name = name
+        self.description = description
+        self.language = language
+        self.lastUpdateOn = lastUpdateOn
+    }
 }
--- Project/Source Files/DataModel/User.swift
+++ Project/Source Files/DataModel/User.swift
@@ -7,15 +7,19 @@
 import Foundation
 
 class User {
-//    private var id : Int
-//    private var userName : String
-//    private var avatar_url : String
-//    private var url : String
-//    private var name : String
-//    private var repositoriesNumber : Int
-//    private var repositories : [Repository]
-//
-//    init() {
-//
-//    }
+    private var id : Int
+    private var userName : String
+    private var avatar_url : String
+    private var url : String
+    private var repositoriesNumber : Int
+    private var repositories : [Repository]
+
+    init(id : Int, userName: String, avatar_url : String, url : String, repositoriesNumber: Int) {
+        self.id = id
+        self.userName = userName
+        self.avatar_url = avatar_url
+        self.url = url
+        self.repositoriesNumber = repositoriesNumber
+        self.repositories = []
+    }
 }
