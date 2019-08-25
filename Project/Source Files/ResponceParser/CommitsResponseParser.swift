4f3064720170df723d7a03f27d3cf1187094d4c3
FilipGrudzinski <filip.grudzinski@b-mind.pl>
2019-08-25 15:10:29 +0200

    small fix

--- Project/Source Files/ResponceParser/CommitsResponseParser.swift
+++ Project/Source Files/ResponceParser/CommitsResponseParser.swift
@@ -22,14 +22,11 @@ final class CommitsResponseParser {
 
     func parseCommitsRequestArray(array: [[String: Any]]) throws -> [CommitsResponse] {
         #warning("not Done yet")
-//        return try array.compactMap { item in
-//            guard let name = item[] as? String,
-//                let status = item["state"] as? String else {
-//                    throw ResponseParserError.wrongResponse
-//            }
-            return CommitsResponse(name: name, date: <#T##Date#>, message: <#T##String#>)
-        }
+        //        return try array.compactMap { item in
+        //            guard let name = item[] as? String,
+        //                let status = item["state"] as? String else {
+        //                    throw ResponseParserError.wrongResponse
+        //            }
+        return CommitsResponse(name: name, date: <#T##Date#>, message: <#T##String#>)
     }
-
-
 }
