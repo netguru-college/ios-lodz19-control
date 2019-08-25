7454679f2e5a360b0dac6dad8774579f3fabad45
Krystian Kozerawski <mackozer@gmail.com>
2019-08-25 12:25:34 +0200

    Login Button View
    Moving update icon method out of init.
    Login icon asset added.

--- Project/SharedUI/LoginButton.swift
+++ Project/SharedUI/LoginButton.swift
@@ -11,21 +11,25 @@ class LoginButtonView: UIView {
     let avatarImageView = UIImageView()
     let userManager: UserManager
     private let size = 28
+    private let loginIcon = "login_icon"
     
     init(userManager: UserManager) {
         self.userManager = userManager
         super.init(frame: CGRect(x: 0, y: 0, width: size, height: size))
         addSubview(avatarImageView)
         avatarImageView.sizeToFit()
-        
+        updateIcon()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func updateIcon() {
         if let avatarUrl = userManager.avatarUrl {
             avatarImageView.image = UIImage(contentsOfFile: avatarUrl)
         } else {
-            avatarImageView.image = nil
+            avatarImageView.image = UIImage(named: loginIcon)
         }
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
 }
